5145e8771e861e550be25d85292bbf08fdc1ee73
Tortoisey1 <88613481+Tortoisey1@users.noreply.github.com>
2024-05-29 20:47:00 +0800

    Added the required buttons in step 4

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-function add(a,b){
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
 }
 
 
 let firstNum, secondNum, operator;
 
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+
+        case '-':
+            return subtract(firstNum,secondNum);
+        
+        case '*':
+            return multiply(firstNum, secondNum);
+
+        case '/':
+            return divide(firstNum, secondNum);
+
+
+    }
+}
