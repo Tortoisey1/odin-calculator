b60f16fb735c27029db9b7c9716a1d6f1b6a9584
Tortoisey1 <88613481+Tortoisey1@users.noreply.github.com>
2024-05-30 12:55:35 +0800

    button logic first try

--- script.js
+++ script.js
@@ -23,14 +23,82 @@ function operate(operator, firstNum, secondNum) {
             return add(firstNum, secondNum);
 
         case '-':
-            return subtract(firstNum,secondNum);
-        
+            return subtract(firstNum, secondNum);
+
         case '*':
             return multiply(firstNum, secondNum);
 
         case '/':
             return divide(firstNum, secondNum);
+        default:
+            return null;
 
 
     }
 }
+
+let displayValue = "0";
+
+let display = document.querySelector(".display");
+
+function reset() {
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+}
+
+function updateDisplay() {
+    display.textContent = displayValue || '0';
+}
+
+function clear() {
+    displayValue = '';
+    reset();
+    updateDisplay();
+}
+
+
+//function for numbers
+document.querySelectorAll(".number").forEach(button => {
+    button.addEventListener("click", () => {
+        displayValue += button.textContent;
+        updateDisplay();
+    });
+});
+
+
+//function for operators
+document.querySelectorAll(".operator").forEach(button => {
+    if (firstNum == null) {
+        firstNum = parseFloat(displayValue);
+    } else {
+        secondNum = parseFloat(displayValue);
+        displayValue = operate(operator, firstNum, secondNum).toString();
+    }
+    operator = button.textContent;
+    displayValue = "";
+    updateDisplay();
+});
+
+
+//function for equal
+document.querySelector("#equal").addEventListener("click", () => {
+    //check if operator and value is present
+    if (operator && displayValue) {
+        secondNum = parseFloat(displayValue);
+        displayValue = operate(operator, firstNum, secondNum);
+        reset();
+        updateDisplay();
+    }
+
+});
+
+
+//function for clear
+document.querySelector("#clear").addEventListener("click", () => {
+    clear();
+});
+
+
+//start the calculator
+updateDisplay();
